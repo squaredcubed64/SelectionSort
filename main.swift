de4730310befbcaeefd53e29f71cb9db158f0000
Jakob McPherson <jakob@mcpherson.org>
2022-10-07 12:51:43 -0500

    Fixed swift file

--- main.swift
+++ main.swift
@@ -1,48 +1,30 @@
+var strings: [String] = []
 
-
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-// 1.  Edit the file named "main.swift"
-// 2.  Within this file, write a program which will sort the array of integers
-//     using Selection Sort.
-// 3.  The output should be as follows:
-//     Pass: <p>, Swaps: <pass swap count>/<total swap count>, Array: [<comma-delimitted array contents>]
-//     For example:
-//         Pass 0, Swaps: 0/0, Array: [64, 25, 12, 22, 11]
-//         Pass 1, Swaps: 1/1, Array: [11, 25, 12, 22, 64]
-
-// Add your code below:
-
-var arr = unsortedIntegers
-
-func swap(arr: inout [Int], index1:Int, index2:Int) {
-    let ele1 = arr[index1]
-    let ele2 = arr[index2]
-    arr[index1] = ele2
-    arr[index2] = ele1
+while let line = readLine() {
+    strings.append(line)
 }
 
-var totalSwaps = 0
+func swap(strings: inout [String], index1:Int, index2:Int) {
+    let ele1 = strings[index1]
+    let ele2 = strings[index2]
+    strings[index1] = ele2
+    strings[index2] = ele1
+}
 
-print("Pass: 0, Swaps: 0/0, Array: \(arr)")
-
-for i in 0 ..< arr.count - 1{
-    var min = arr[i]
+for i in 0 ..< strings.count - 1 {
+    var min = strings[i]
     var minIndex = i
     
-    for j in i + 1 ..< arr.count {
-        if arr[j] < min {
-            min = arr[j]
+    for j in i + 1 ..< strings.count {
+        if strings[j] < min {
+            min = strings[j]
             minIndex = j
         }
     }
 
-    swap(arr:&arr, index1:i, index2:minIndex)
-    totalSwaps += 1
-   
-    print("Pass: \(i + 1), Swaps: 1/\(totalSwaps), Array: \(arr)")
+    swap(strings:&strings, index1:i, index2:minIndex)
+}
+
+for str in strings {
+    print(str)
 }
